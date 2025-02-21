7d17cd35635fd7005e5b35bd90749d4b5edf5019
Gilberto Santos <gsantos@g-p.com>
2025-02-21 11:38:13 +0000

    prettier notfound id

--- jobtracker/src/app/applications/[id]/page.tsx
+++ jobtracker/src/app/applications/[id]/page.tsx
@@ -1,18 +1,17 @@
-import { useRouter } from "next/router";
+
 import { Job } from "@/model/application"
 
-export default async function ApplicationDetail(){
- const { id } = context.params!;
-  const res = await fetch(`http://localhost:5000/applications/${id}`); // Fetch from JSON server
+export default async function ApplicationDetail({ params }:{ params: {id: string}}){
+ 
+  const res = await fetch(`http://localhost:5000/applications/${params.id}`); // Fetch from JSON server
   const job: Job = await res.json();
-  const router = useRouter();
+ 
 
   if (!job) return <p>Loading...</p>;
 
     return(
         <div className="max-w-2xl mx-auto p-6 border rounded-lg shadow-lg">
         <button
-          onClick={() => router.push("/applications")}
           className="mb-4 px-4 py-2 bg-gray-200 hover:bg-gray-300 rounded"
         >
           ← Back to Jobs
